57d1d0f2a72abe3f88433c6095e5df33c1a3d2bc
Shantanu <shantanu@Shantanus-MBA.bbrouter>
2023-01-10 13:52:38 +0530

    Added view set based class for products

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('<int:pk>', views.ProductDetailsAPIView.as_view()),
     path('', views.ProductListCreateAPIView.as_view()),
+    path('products/', views.ProductListCreateAPIViewSet.as_view({'get': 'list', 'post': 'create'}))
+    # path('apple-products', views.AppleProductListModelMixin.as_view())
     # path('', views.index),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,17 +2,20 @@ from django.http.response import JsonResponse
 from django.forms.models import model_to_dict
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import generics
+from rest_framework.decorators import api_view, action
+from rest_framework import generics, viewsets
+from rest_framework.mixins import ListModelMixin
+from rest_framework import permissions
 
 from products.models import Product
 from products.serializers import ProductSerializer
 
 # Create your views here.
 
+
 @api_view(['GET', 'POST'])
 def index(request, *args, **kwargs):
-    if request.method == 'GET':        
+    if request.method == 'GET':
         instance = Product.objects.all().order_by('?').first()
         data = {}
 
@@ -20,7 +23,7 @@ def index(request, *args, **kwargs):
             data = ProductSerializer(instance).data
 
         return Response(data)
-    elif request.method == 'POST':        
+    elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             print(serializer.validated_data)
@@ -33,11 +36,31 @@ def index(request, *args, **kwargs):
             return Response({'details': 'Invalid Data'})
 
 
-
 # special generic views that django offers
 class ProductDetailsAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductListCreateAPIViewSet(viewsets.GenericViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def list(self, request):
+        query_set = Product.objects.order_by('-company')
+        serializer = ProductSerializer(query_set, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            company_name = serializer.validated_data.get('company')
+            if company_name.lower() == 'apple':
+                return Response({'details': 'Please, no Apple products allowd'}, status=403)
+            serializer.save()
+        else:
+            return Response({'details': 'Invalid Data'})
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -55,9 +78,23 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         company = serializer.validated_data.get('company')
         print(company)
         if company.upper() == 'APPLE':
-            raise generics.ValidationError('No Apple products are allowed in store')
+            # raise generics.ValidationError('No Apple products are allowed in store')
+            return Response('Failed to save product. No Apple devices allowd', status=403)
         else:
             instance = serializer.save()
+
+
+# Mixin based classes
+
+class AppleProductListModelMixin(ListModelMixin, generics.GenericAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        company = request.query_params.get('company')
+        if company:
+            self.queryset = self.queryset.filter(company__iexact=company)
+        return super().list(request, *args, **kwargs)
 
 
 # The typical django way of writing view functions.
